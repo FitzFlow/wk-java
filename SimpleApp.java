5659804407d0436dcfe058b9db47e79eb49c75a4
FitzFlow <florian.etheve@epitech.eu>
2023-06-23 11:21:06 +0400

    Push some code

---
+++ SimpleApp.java
@@ -0,0 +1,5 @@
+public class SimpleApp {
+    public static void main(String[] arg) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ WorkshopJava-app/src/main/java/com/WorkshopJava/app/App.java
@@ -0,0 +1,62 @@
+package com.WorkshopJava.app;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.entity.Entity;
+import javafx.scene.input.KeyCode;
+import java.util.Map;
+
+public class App extends GameApplication {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("WorkshopJava");
+    }
+    private Entity player;
+    private Entity ennemy;
+    @Override
+    protected void initGame() {
+        int pos_x = 0;
+        int pos_y = 0;
+        FXGL.getGameWorld().addEntityFactory(new Factory());
+        player = FXGL.entityBuilder()
+            .at(pos_x, pos_y)
+            .view(new Rectangle(25, 25, Color.BLUE))
+            .buildAndAttach();
+        ennemy = FXGL.spawn("enemy", 5, 5);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = FXGL.getInput();
+
+        FXGL.onKey(KeyCode.LEFT, () -> {
+            player.translateX(-5);
+            ennemy.translateX(-3);
+        });
+        FXGL.onKey(KeyCode.RIGHT, () -> {
+            player.translateX(5);
+            ennemy.translateX(3);
+        });
+        FXGL.onKey(KeyCode.UP, () -> {
+            player.translateY(-5);
+            ennemy.translateY(-3);
+        });
+        FXGL.onKey(KeyCode.DOWN, () -> {
+            player.translateY(5);
+            ennemy.translateY(3);
+        });
+    }
+}
